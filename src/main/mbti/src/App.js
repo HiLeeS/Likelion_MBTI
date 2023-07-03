ec3b235d1d28197828b4998a607a7685b4105416
HiLeeS <azs8017@naver.com>
2023-07-03 21:49:29 +0900

    연결 완료

--- src/main/mbti/src/App.js
+++ src/main/mbti/src/App.js
@@ -2,60 +2,72 @@ import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Result from './pages/Result';
 import Admin from './pages/Admin';
-import React, {useState} from 'react'; 
+import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
 
-  let [input,setinput] = useState(''); //사용자가 입력한 mbti 저장
-  let [output,setoutput] = useState('');
-  let navigater = useNavigate();
-  return (
-<>
-    <div className="App">
-            <header>MBTI</header>
-    <Routes>
-        <Route path='/' element={ 
-          
-              <div className='container'>
-              <div id="mbti_btns">
-                <button className="btn" id="E">E</button>
-                <button className="btn" id="S">S</button>
-                <button className="btn" id="F">F</button>
-                <button className="btn" id="P">P</button>
-                <button className="btn" id="I">I</button>
-                <button className="btn" id="N">N</button>
-                <button className="btn" id="T">T</button>
-                <button className="btn" id="J">J</button>
+//사용자가 입력한 mbti 저장
+    let [output,setOutput] = useState('');
+    let navigater = useNavigate();
+    let [click, setClick] = useState(['','','','']);
+    let [input, setInput] = useState('');
+
+    function 바꿔(i,mbti){
+        var newArray = [...click];
+        newArray[i] = mbti;
+        setClick(newArray);
+    }
+
+    return (
+        <>
+            <div className="App">
+                <header>MBTI</header>
+                <Routes>
+                    <Route path='/' element={
+                        <div className='container'>
+                            <div id="mbti_btns">
+                                <button onClick={ ()=> 바꿔(0,'E') } className={ click[0] =='E' ? 'click' : 'btn'} id="E">E</button>
+                                <button onClick={ ()=> 바꿔(1,'S') } className={ click[1] =='S' ? 'click' : 'btn'} id="S">S</button>
+                                <button onClick={ ()=> 바꿔(2,'F') } className={ click[2] =='F' ? 'click' : 'btn'} id="F">F</button>
+                                <button onClick={ ()=> 바꿔(3,'P') } className={ click[3] =='P' ? 'click' : 'btn'} id="P">P</button>
+                                <button onClick={ ()=> 바꿔(0,'I') } className={ click[0] =='I' ? 'click' : 'btn'} id="I">I</button>
+                                <button onClick={ ()=> 바꿔(1,'N') } className={ click[1] =='N' ? 'click' : 'btn'} id="N">N</button>
+                                <button onClick={ ()=> 바꿔(2,'T') } className={ click[2] =='T' ? 'click' : 'btn'} id="T">T</button>
+                                <button onClick={ ()=> 바꿔(3,'J') } className={ click[3] =='J' ? 'click' : 'btn'} id="J">J</button>
+                            </div>
+
+                            <button id="confirm" onClick={ ()=>{
+                                console.log(`${click[0]}`+`${click[1]}`+`${click[2]}`+`${click[3]}`);
+                                setInput(`${click[0]}`+`${click[1]}`+`${click[2]}`+`${click[3]}`);
+                                axios.get(`http://localhost:8080/mbti/${click[0]+click[1]+click[2]+click[3]}`)
+                                    .then( (data)=> {
+                                        console.log(data.data.info);
+                                        setOutput(data.data.info);
+                                    })
+                                    .catch( ()=>{
+                                        console.log('실패함');
+                                    })
+                                navigater('./result');
+                            }}>확인</button>
+
+                        </div>
+                    } />
+                    <Route path='/result' element={ <Result input={input} output={output}/>} />
+                    <Route path='/admin' element={ <Admin/> } />
+                    <Route path='*' element={ <div>없는페이지요</div>} />
+
+                </Routes>
+
             </div>
-            <input onChange={ (e)=>{setinput(e.target.value); console.log(input)}}/><br/>
-            <button id="confirm" onClick={ ()=>{ 
-              axios.get(`http://localhost:8080/mbti/${input}`)
-              .then( (data)=> {
-                console.log(data.data.info);
-                setoutput(data.data.info);
-               })
-               .catch( ()=>{
-                console.log('실패함');
-               })
-              navigater('./result');
-            }}>확인</button>
 
-        </div>
-         } /> 
-        <Route path='/result' element={ <Result input={input} output={output}/>} />
-        <Route path='/admin' element={ <Admin/> } />
-        <Route path='*' element={ <div>없는페이지요</div>} /> 
+            <footer>
+            </footer>
+        </>
 
-      </Routes>
+    );
 
-      </div>
-    
-    <footer>
-    </footer>
-    </>
-    
-  );
+
 }
 
 export default App;
--- src/main/mbti/src/pages/Admin.js
+++ src/main/mbti/src/pages/Admin.js
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import React, {useState} from 'react'; 
+import React, {useState} from 'react';
 import axios from 'axios';
 
 let InputBox = styled.input`
-    width : ${ props => props.width };
-    height :${ props => props.height };;
-    color :  black ;
-    border-radius: 3px;
-    border : 1px solid black;
-    margin-top: 0px;
-    margin-bottom : 20px;
+  width : ${ props => props.width };
+  height :${ props => props.height };;
+  color :  black ;
+  border-radius: 3px;
+  border : 1px solid black;
+  margin-top: 0px;
+  margin-bottom : 20px;
 `
 let Flex = styled.div`
-  width : 70%;  
+  width : 70%;
 `
 let Text = styled.h3`
-    margin-bottom : 5px;
-    font-size : ${ props => props.size };
+  margin-bottom : 5px;
+  font-size : ${ props => props.size };
 `
 
 let MyBtn = styled.button`
@@ -31,10 +31,10 @@ let MyBtn = styled.button`
 
 function Admin(){
 
-  let navigater = useNavigate();
+    let navigater = useNavigate();
 
-  let [input,setinput] = useState('');
-  let [content,setcontent] = useState('');
+    let [input,setinput] = useState('');
+    let [content,setcontent] = useState('');
 
     return(
         <div className="container">
@@ -43,25 +43,30 @@ function Admin(){
             <Text size='14px'>성향추가</Text>
             <InputBox width='400px' height='30px' onChange={ (e)=>{ setcontent(e.target.value); }}></InputBox>
             <Flex>
-              <MyBtn onClick={ ()=>{ 
-                //post요청
-                axios.post('http://localhost:8080/mbti', { mbti : input , info : content})
-              .then( (data)=> {
-                //완료시
-                alert('서버 전송 성공');
-                console.log('0');
-               })
-               .catch( ()=>{
-                //실패시
-                console.log('실패함');
-               })
-              
-                
+                <MyBtn onClick={ ()=>{
+                    //post요청
+                    axios.post('http://localhost:8080/mbti', { mbti : input , info : content})
+                        .then( (data)=> {
+                            //완료시
+                            alert('서버 전송 성공');
+                            console.log('0');
+                        })
+                        .catch( ()=>{
+                            //실패시
+                            console.log('실패함');
+                        })
+
+
                 }}>특징 추가</MyBtn>
-            </Flex>  
-        </div> 
+                <br/>
+                <br/>
+                <MyBtn>
+                    <a href="http://localhost:8080/mbti">전체 데이터 확인</a>
+                </MyBtn>
+            </Flex>
+        </div>
     )
-  }
-  
+}
+
 export default Admin;
   
